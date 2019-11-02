1691d1b25520770152286b1d5729c89e31c1ea9b
chrisriker <cdr11c@my.fsu.edu>
2019-11-02 19:47:29 -0400

    Update RaidBot.java

--- src/main/java/RaidBot.java
+++ src/main/java/RaidBot.java
@@ -6,6 +6,7 @@ import javax.security.auth.login.LoginException;
 // Fun ideas for stuff:
 // 1) Keep raid rosters and their classes; dump a report of all of the
 //    members in the raid and the raids party protects/buffs etc.
+// 2) Auto ping members 1h or 30m before raid
 
 public class RaidBot {
     public void addRaid(Raid aRaid)
